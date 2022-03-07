70878bd1f1ba31acd9b40b8cec4a105164d64a6e
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2022-03-08 01:02:35 +0900

    change database definition

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,5 +4,5 @@ DATABASE = 'database.db'
 
 def create_books_table():
     con = sqlite3.connect(DATABASE)
-    con.execute("CREATE TABLE IF NOT EXISTS books (title, price, arrival_day)")
+    con.execute("CREATE TABLE IF NOT EXISTS books (title, price, year, month, day)")
     con.close()
